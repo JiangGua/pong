bdfb32443341b2ea4f58bcd2bfe6c4fc48e7f8fa
JiangGua <jonbgua@gmail.com>
2020-06-04 09:48:03 +0800

    简易统计数据API：/api/stats

--- pong/app.py
+++ pong/app.py
@@ -25,14 +25,21 @@ def ping():
     else:
         return "Use POST method instead"
         
-@app.route('/stats')
+@app.route('/stats/')
 def stats():
-    links = list(db_links.find({}, sort=[("count", pymongo.DESCENDING)]))
+    links = list(db_links.find({}, sort=[("count", {'_id': False}, pymongo.DESCENDING)]))
     config = {
         'title': os.environ.get('SITE_TITLE', default='Stats - Pong'),
         'h1': os.environ.get('SITE_TITLE', default=None) or os.environ.get('SITE_TITLE', default='Stats - Pong')
     }
     return render_template('stats.html', config=config, links=links)
+
+@app.route('/api/stats/')
+def api_stats():
+    links = list(db_links.find({}, {'_id': False}, sort=[("count", pymongo.DESCENDING)]))
+    return {
+        'links': links
+    }
 
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", port = 5000)
